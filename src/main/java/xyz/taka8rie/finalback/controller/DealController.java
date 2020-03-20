1077a9163c3aea40442b76a80959321035dd10fa
taka8rie <614877479@qq.com>
2020-03-20 15:13:22 +0800

    试租房的流程

--- src/main/java/xyz/taka8rie/finalback/controller/DealController.java
+++ src/main/java/xyz/taka8rie/finalback/controller/DealController.java
@@ -1,10 +1,7 @@
 package xyz.taka8rie.finalback.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import xyz.taka8rie.finalback.Service.DealService;
 import xyz.taka8rie.finalback.pojo.Deal;
 
@@ -14,7 +11,7 @@ public class DealController {
     DealService dealService;
 
     @CrossOrigin
-    @GetMapping("api/dealadd")
+    @PostMapping ("api/dealadd")//这里本来用getMapping
     public Deal addOrUpdate(@RequestBody Deal deal) {
         System.out.println("进入了添加订单的方法");
         dealService.addOrUpdate(deal);

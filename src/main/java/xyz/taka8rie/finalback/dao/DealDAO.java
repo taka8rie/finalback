ce0fde567a4773d37219923775b7da12e010756c
taka8rie <614877479@qq.com>
2020-05-06 16:58:50 +0800

    增加按照时间段来查找相应订单

--- src/main/java/xyz/taka8rie/finalback/dao/DealDAO.java
+++ src/main/java/xyz/taka8rie/finalback/dao/DealDAO.java
@@ -6,6 +6,7 @@ import xyz.taka8rie.finalback.pojo.Deal;
 import xyz.taka8rie.finalback.pojo.House;
 
 import javax.management.ValueExp;
+import java.sql.Date;
 import java.util.List;
 
 public interface DealDAO extends JpaRepository<Deal,Integer> {
@@ -19,4 +20,11 @@ public interface DealDAO extends JpaRepository<Deal,Integer> {
     //找出某一房屋对应的dealID
     @Query(value = "select deal_number from deal where house_number=?1", nativeQuery = true)
     Integer findDealNumberByHouseNumber(int houseNumber);
+
+    //通过dealNumber查找对象
+    Deal findAllByDealNumber(int DealNumber);
+
+    //5.6 统计某个时间段的订单数量
+    List<Deal> findAllByHandleTimeBetween(Date time1, Date time2);
+
 }
